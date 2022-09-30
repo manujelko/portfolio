8f732a79d4d7a4e8a98ffd340e4cf837aa742884
Manuel Šarić <manujelko@gmail.com>
2022-09-30 20:54:14 +0200

    Making our flask app for this project

---
+++ portfolio/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html.j2")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html.j2")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.j2")
