95694713f695a7dee7334cd0c431f3cb243e8fc6
Manuel Šarić <manujelko@gmail.com>
2022-10-01 13:43:52 +0200

    Creating individual project pages

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 
 app = Flask(__name__)
@@ -28,6 +28,8 @@ projects = [
     },
 ]
 
+slug_to_project = {project["slug"]: project for project in projects}
+
 
 @app.route("/")
 def home():
@@ -42,3 +44,10 @@ def about():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/projects/<string:slug>/")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
